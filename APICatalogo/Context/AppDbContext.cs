a0a8e86070d73914e397c7b3724941217343b1da
Julio Oliveira <juliocesar.jc585@gmail.com>
2023-03-01 15:21:24 -0300

    Creation archive Context and class for DbContext

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace APICatalogo.Context
+{
+    public class AppDbContext
+    {
+    }
+}
--- APICatalogo/Models/Product.cs
+++ APICatalogo/Models/Product.cs
@@ -1,6 +1,15 @@
-﻿namespace APICatalogo.Models
+﻿namespace APICatalogo.Models;
+
+public class Product
 {
-    public class Product
-    {
-    }
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public float stock { get; set; }
+
+    public DateTime DateRegistration { get; set; }
 }
