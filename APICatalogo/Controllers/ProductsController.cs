96d70667d7f50d553a695ad1c1a32659cc3ade49
Julio Oliveira <juliocesar.jc585@gmail.com>
2023-03-13 17:13:24 -0300

    Created method get(action)

--- APICatalogo/Controllers/ProductsController.cs
+++ APICatalogo/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,31 @@ namespace APICatalogo.Controllers
         {
             _context = context;
         }
+        // criação do método action  que retornam dados, ou seja, métodos que usam  metodo HTTP GET
 
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var products = _context.Products.ToList();
+            if (products is null)
+            {
+                return NotFound("Produtos não encontrados!"); //Método action é retorna um tipo ActionResult
+            }
+            return products;
+        }
+        //metodo Get para retorno dos produtos pelo ID
+        //Necessário repassa o ID pelo REQUEST
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Product> ProductGet(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p=>p.ProductId== id);
+            if (product == null)
+            {
+                return NotFound("Produto não encontrado!");
+            }
+            return product;
+        }
     }
 }
+
