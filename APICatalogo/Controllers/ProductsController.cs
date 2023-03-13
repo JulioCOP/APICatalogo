05a07208708e3c7bd6ab89ac2ccb8210e7e8c0c7
Julio Oliveira <juliocesar.jc585@gmail.com>
2023-03-13 16:43:23 -0300

    Create class ProductsController

---
+++ APICatalogo/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")] //Rota da requisição para acessar o controlador, sendo necessário informar apenas a URL "Produtos"
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context; //apenas leitura da calsse db context
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
